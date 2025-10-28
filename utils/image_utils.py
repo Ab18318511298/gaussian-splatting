3d04d09d0e19626c2575b7e03657c8a2bb73c507
Ab18318511298 <3495809477@qq.com>
2025-10-28 16:28:10 +0800

    Update image_utils.py

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -11,9 +11,11 @@
 
 import torch
 
+# 定义图像质量指标（均方误差：Mean Squared Error）
 def mse(img1, img2):
     return (((img1 - img2)) ** 2).view(img1.shape[0], -1).mean(1, keepdim=True)
 
+# 定义图像质量指标（峰值信噪比：Peak Signal-to-Noise Ratio）
 def psnr(img1, img2):
     mse = (((img1 - img2)) ** 2).view(img1.shape[0], -1).mean(1, keepdim=True)
     return 20 * torch.log10(1.0 / torch.sqrt(mse))
