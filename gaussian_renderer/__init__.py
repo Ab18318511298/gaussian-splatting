622cd5cfdb02ba949b6d78e6a897332461503553
Ab18318511298 <3495809477@qq.com>
2025-10-28 16:23:17 +0800

    Update __init__.py

--- gaussian_renderer/__init__.py
+++ gaussian_renderer/__init__.py
@@ -8,7 +8,7 @@
 #
 # For inquiries contact  george.drettakis@inria.fr
 #
-transform_point
+
 import torch
 import math
 from diff_gaussian_rasterization import GaussianRasterizationSettings, GaussianRasterizer
